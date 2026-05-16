d44fca4e98e32981a361b1664fe901dbf06c72a8
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2026-05-16 19:51:14 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="aside">
+        <SideBar />
+      </div>
+      <div className="main"></div>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import { Car } from "lucide-react";
+
+function SideBar() {
+  return (
+    <div>
+      <div>
+        <h2>CR</h2>
+      </div>
+      <div>
+        <div>
+          <Car />
+          <span>Cars</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SideBar;
