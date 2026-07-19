d151870ad6cd5b9429063a73f27ad20c8aaa610b
Mohamed Amine Nafia <aminenafia05@gmail.com>
2026-07-19 19:53:10 +0200

    Update Cars.jsx

--- website/src/components/Cars.jsx
+++ website/src/components/Cars.jsx
@@ -31,7 +31,7 @@ function Cars({ isDarkMode, language }) {
   useEffect(() => {
     const fetchCars = async () => {
       try {
-        const response = await fetch(`${API_URL}/fetch-available-cars.php`);
+        const response = await fetch(`https://car-rental.free.je/car_rental/fetch-available-cars.php`);
         const data = await response.json();
 
         if (data.status !== "success") {
